71944433da31244dcb48765dbbee2488bc4c1326
Tata2222 <tataolcha@gmail.com>
2020-05-26 00:48:35 +0300

    Updates

---
+++ precache-manifest.0bc12e9efde7016b48afd41699180834.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518e11c05c22171d6974c952b5cd3e3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf559f8d8bba89531965",
+    "url": "./static/css/main.7156d396.chunk.css"
+  },
+  {
+    "revision": "a7cbad63465172db8f17",
+    "url": "./static/js/2.d962a46c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d962a46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf559f8d8bba89531965",
+    "url": "./static/js/main.4e9d07e2.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "5660663cd1d5d183e625d3e9a2a5c5d3",
+    "url": "./static/media/Mont-Bold.5660663c.woff"
+  },
+  {
+    "revision": "31fcd73a761b439cf0e1a9f7462de22a",
+    "url": "./static/media/Mont-Regular.31fcd73a.woff"
+  },
+  {
+    "revision": "cea85a7bae053795ef0901513d041398",
+    "url": "./static/media/accessories.cea85a7b.jpg"
+  },
+  {
+    "revision": "f952b32750c078ea88834b91689ba1e9",
+    "url": "./static/media/phones.f952b327.jpg"
+  },
+  {
+    "revision": "7c43b4779262715a51108402fa6ed0dc",
+    "url": "./static/media/tablets.7c43b477.jpg"
+  }
+]);
